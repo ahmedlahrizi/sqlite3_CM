fa6c2a0ffdc221fd1bf85bcc554034fdc379113b
Ahmed Lahrizi <ahmed.lahrizi05@gmail.com>
2021-06-15 09:38:04 +0000

    changed some typos addd stuff to readme and fixed a throw error bug and added setup.py for pip distribution (not done yet)

--- sqlite3cm/sqlite3cm.py
+++ sqlite3cm/sqlite3cm.py
@@ -1,8 +1,34 @@
+"""A module with a class called OpenSqlite3db that handles sqlite 3 db with the "with" statement """
+
 import sqlite3
 
 
 class OpenSqlite3db:
-    def __init__(self, db_path="database.db", throw_error=False):
+    """
+    The classe that you can with the open statement to open SQLite databases
+
+    Examples:
+        with OpenSqlite3db(db_path="database.db", throw_error=True) as (conn, cursor):
+            conn.execute('''
+                CREATE TABLE IF NOT EXISTS users
+            ''')
+
+        NOTE:
+            It will automatically commit, close cursor and connection
+
+    Documentation:
+        You shouldn't forget about the parentisis after the "as"
+
+        Arguments:
+            "db_path" Path to the database (can be Pathlib path) (default to "database.db")
+
+            "throw_error" Weather you want to throw an error or not (default to True)
+
+        Note:
+            None of these arguments are required
+
+    """
+    def __init__(self, db_path="database.db", throw_error=True):
         self.db = str(db_path)
         self.throw_error = throw_error
 
@@ -12,7 +38,9 @@ class OpenSqlite3db:
         return self.connection, self.cursor
 
     def __exit__(self, ex_type, exc_val, exc_tb):
+        # Commiting and closing cursors
         self.connection.commit()
         self.cursor.close()
         self.connection.close()
-        return self.throw_error
+        # The boolean that __exit__ returns is weather or not it'll catch errors
+        return not self.throw_error
